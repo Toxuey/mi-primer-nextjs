c577639d9583df88e1313452d040442eebc29cda
Cristian Villamizar <cristian.9425@hotmail.es>
2025-10-22 12:20:39 -0500

    About creado, carpeta y codigo

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function ContactPage (){
+    return(
+        <main className="p-10 text-center">
+            <h1 className="text-2xl font-semibold text-purple-600">
+                Acerca de mi
+            </h1>
+            <p className="mt-3 text-gray-700">
+                Esta es la pagina about de mi primer proyectos Next.js
+            </p>
+        </main>
+    );
+}
